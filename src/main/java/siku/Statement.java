7dc6e85686c3636b800f31b3deea87c07909fb0a
SIKU-KR <peter012677@naver.com>
2025-01-23 20:44:39 +0900

    Feature
    1. HTML 관련 렌더링 메서드 추가

--- src/main/java/siku/Statement.java
+++ src/main/java/siku/Statement.java
@@ -23,6 +23,24 @@ public class Statement {
         return result;
     }
 
+    public String htmlStatement(Invoice invoice, HashMap<String, Play> plays) {
+        return renderHtml(StatementDataFactory.createStatementData(invoice, plays));
+    }
+
+    private String renderHtml(StatementData statementData) {
+        String result = "<h1>청구 내역 (고객명: " + statementData.getCustomer() + ")</h1>\n";
+        result += "<table>\n";
+        result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>\n";
+        for (EnrichPerformance performance : statementData.getPerformances()) {
+            result += "<tr><td>" + performance.getPlay().getName() + "</td><td>" + performance.getAudience() + "</td>";
+            result += "<td>" + usd(performance.getAmount()) + "</td></tr>\n";
+        }
+        result += "</table>\n";
+        result += "<p>총액: <em>" + usd(statementData.getTotalAmount()) + "</em></p>\n";
+        result += "<p>적립 포인트: <em>" + statementData.getTotalVolumeCredits() + "</em>점</p>\n";
+        return result;
+    }
+
     private String usd(double amount) {
         return String.format("%.2f", amount / 100.0);
     }
